de32204fc09e639e37eef5f539575ac91f003790
Krypton91 <krypton1@gmx.at>
2022-08-26 11:48:34 +0200

    removed zone layout.

--- scripts/3_Game/GUI/KOTH_AdminMenu.c
+++ scripts/3_Game/GUI/KOTH_AdminMenu.c
@@ -15,22 +15,8 @@ class KR_KOTH_ADMIN_UI extends UIScriptedMenu
     protected Widget                                m_EventsTab;
     protected Widget                                m_ZonesTab;
 
-    //Popup
-    protected Widget                                m_ManageEventPopup;
-    protected EditBoxWidget                         m_ManageEventPosX;
-    protected EditBoxWidget                         m_ManageEventPosY;
-    protected EditBoxWidget                         m_ManageEventPosZ;
-    protected EditBoxWidget                         m_ManageEventCPRad;
-    protected EditBoxWidget                         m_ManageEventEventRad;
-    protected EditBoxWidget                         m_ManageEventEventAiAmount;
-    protected EditBoxWidget                         m_ManageEventEventZoneName;
-    protected EditBoxWidget                         m_ManageEventEventLootTier;
-    protected EditBoxWidget                         m_ManageEventEventLootObjectList;
-    protected ButtonWidget                          m_StartForceEventBtn;
-
     protected MapWidget                             m_Map;
-
-    autoptr KR_KingOfTheHillLocation                m_ForeceLocation;
+    protected autoptr KR_KOTH_POPUPEditor           m_PopupWindow;
 
     override Widget Init()
     {
@@ -42,21 +28,9 @@ class KR_KOTH_ADMIN_UI extends UIScriptedMenu
             m_TabZonesBtn                       = ButtonWidget.Cast(layoutRoot.FindAnyWidget("BtnConfigManager"));
             m_RewardsBtn                        = ButtonWidget.Cast(layoutRoot.FindAnyWidget("BtnRewardsManager"));
             m_MiscsBtn                          = ButtonWidget.Cast(layoutRoot.FindAnyWidget("BtnMiscsManager"));
-            m_StartForceEventBtn                = ButtonWidget.Cast(layoutRoot.FindAnyWidget("BtnForceStartEvent"));
             m_Map                               = MapWidget.Cast(layoutRoot.FindAnyWidget("Map"));
             m_EventsTab                         = Widget.Cast(layoutRoot.FindAnyWidget("EventsTab"));
             m_ZonesTab                          = Widget.Cast(layoutRoot.FindAnyWidget("Zones"));
-            m_ManageEventPopup                  = Widget.Cast(layoutRoot.FindAnyWidget("PanelHandleEvent"));
-
-            m_ManageEventPosX                   = EditBoxWidget.Cast(layoutRoot.FindAnyWidget("XPosBox"));
-            m_ManageEventPosY                   = EditBoxWidget.Cast(layoutRoot.FindAnyWidget("YPosBox"));
-            m_ManageEventPosZ                   = EditBoxWidget.Cast(layoutRoot.FindAnyWidget("ZPosBox"));
-            m_ManageEventCPRad                  = EditBoxWidget.Cast(layoutRoot.FindAnyWidget("ZPosBox0"));
-            m_ManageEventEventRad               = EditBoxWidget.Cast(layoutRoot.FindAnyWidget("ZPosBox1"));
-            m_ManageEventEventAiAmount          = EditBoxWidget.Cast(layoutRoot.FindAnyWidget("AiAmountBox"));
-            m_ManageEventEventZoneName          = EditBoxWidget.Cast(layoutRoot.FindAnyWidget("AiAmountBox0"));
-            m_ManageEventEventLootTier          = EditBoxWidget.Cast(layoutRoot.FindAnyWidget("AiAmountBox1"));
-            m_ManageEventEventLootObjectList    = EditBoxWidget.Cast(layoutRoot.FindAnyWidget("AiAmountBox2"));
 
             m_IsMenuInitialized = true;
             m_ActiveTab = m_EventsTab;
@@ -84,10 +58,6 @@ class KR_KOTH_ADMIN_UI extends UIScriptedMenu
                 SwitchTabWithAnimation(m_ZonesTab);
                 break;
             }
-            case m_StartForceEventBtn:
-            {
-                ForceAdminHill();
-            }
         }
 
         return super.OnClick(w, x, y, button);
@@ -109,54 +79,13 @@ class KR_KOTH_ADMIN_UI extends UIScriptedMenu
         if(m_Map)
         {
             vector pos = m_Map.ScreenToMap(Vector(x, y, 0));
-            OpenPanelHandleEvent(pos);
             m_Map.ClearUserMarks();
             m_Map.AddUserMark(pos, "FORCE HILL POSTION", ARGB(255, 255, 0, 0), "KR_KingOfTheHill/GUI/images/FlagIcon.edds");
+            m_PopupWindow = new KR_KOTH_POPUPEditor(m_Map);
+            if(m_PopupWindow){
+                m_PopupWindow.OpenEditor(x, y);
+            }
         }
-    }
-
-    void ForceAdminHill()
-    {
-        string ZoneName = m_ManageEventEventZoneName.GetText();
-        float Radius = m_ManageEventEventRad.GetText().ToFloat();
-        float CaptureRadius = m_ManageEventCPRad.GetText().ToFloat();
-        bool ResetCaptureTimeAfterAllPlayersLeavedZone = false;
-        vector Position = Vector(m_ManageEventPosX.GetText().ToFloat(), m_ManageEventPosY.GetText().ToFloat(), m_ManageEventPosZ.GetText().ToFloat());
-        int AISpawnCount = m_ManageEventEventAiAmount.GetText().ToInt();
-        int LootTier    = m_ManageEventEventAiAmount.GetText().ToInt();
-        string ObjectListName = m_ManageEventEventLootObjectList.GetText();
-
-        m_ForeceLocation = new KR_KingOfTheHillLocation(ZoneName, Radius, CaptureRadius, ResetCaptureTimeAfterAllPlayersLeavedZone, Position, AISpawnCount, LootTier, ObjectListName);
-
-        //Send RPC to server.
-
-    }
-
-    void OpenPanelHandleEvent(vector initPos)
-    {
-        float X, Y, Z;
-        X = initPos[0]
-        Y = initPos[1]
-        Z = initPos[2]
-
-        m_ManageEventPosX.SetText(X.ToString());
-        m_ManageEventPosY.SetText(Y.ToString());
-        m_ManageEventPosZ.SetText(Z.ToString());
-        m_ManageEventCPRad.SetText("20");
-        m_ManageEventEventRad.SetText("150");
-        m_ManageEventEventAiAmount.SetText("10");
-        m_ManageEventEventZoneName.SetText("Admin Force Zone");
-        m_ManageEventEventLootTier.SetText("1");
-        m_ManageEventEventLootObjectList.SetText("none");
-        m_ManageEventPopup.Show(true);
-    }
-
-    enum TabControllerTab
-    {
-        EVENTS = 0,
-        ZONES, 
-        REWARDS,
-        MISCS
     }
 
     int currentTransitionProgress;
